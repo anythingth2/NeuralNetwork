356aab275e911b6d2c8c165f3db2dcc733277636
ChiChaChai <chatchaishaetan@gmail.com>
2019-06-04 23:51:01 +0700

    Add full model

--- model.py
+++ model.py
@@ -2,6 +2,8 @@ import numpy as np
 from functools import reduce
 count_layer = 0
 
+learning_rate = 0.001
+
 
 class Layer:
     def __init__(self, name=None):
@@ -19,6 +21,9 @@ class Layer:
         pass
 
     def forward(self, x):
+        pass
+
+    def fit(self, x, y_true):
         pass
 
     def add_layer(self, layer):
@@ -44,54 +49,87 @@ class Layer:
             self.next_layer.summary()
 
 
-class Input(Layer):
-    def __init__(self, shape):
-        super().__init__()
-        self.output_shape = shape
-
-    def forward(self, x):
-        if self.next_layer != None:
-            return self.next_layer.forward(x)
-
-
 class Dense(Layer):
 
     def __init__(self, size):
         super().__init__()
-
+        self.weight = None
         self.size = size
 
     @property
     def number_variable(self):
-        return reduce(lambda a, b: a*b, self.weight.shape)
+        if self.weight is not None:
+            return reduce(lambda a, b: a*b, self.weight.shape)
+        else:
+            return 0
 
     def compile(self):
         print(f'compiling {self.name}')
-        self.weight = np.random.randn(self.size,
-                                      self.previous_layer.output_shape[0], )
-
-        self.output_shape = (self.size,)
         if self.next_layer != None:
             self.next_layer.compile()
+            self.weight = np.random.randn(self.next_layer.output_shape[0], self.size,
+                                          )
+            print(f'weight shape {self.weight.shape}')
+            self.output_shape = (self.next_layer.size,)
+        else:
+            self.output_shape = (self.size,)
 
     def forward(self, x):
+        if self.next_layer == None:
+            return x
         y = np.empty(self.output_shape)
+
         for index, node in enumerate(y):
             y[index] = np.dot(x, self.weight[index].T)
+
+        y = self.next_layer.forward(y)
+
         return y
+
+    def calculate_loss(self, y_true, y_pred):
+        return np.mean((y_true-y_pred)**2)
+
+    def calculate_gradient(self, y_true, y_pred):
+        return 2*((y_pred-y_true))/y_true.shape[-1]
+
+    def optimise(self, gradient):
+        self.weight = self.weight - learning_rate * gradient
+
+    def fit(self, x, y_true):
+        y_pred = self.forward(x)
+        loss = self.calculate_loss(y_true, y_pred)
+        print(f'loss {loss}')
+        gradient = self.calculate_gradient(y_true, y_pred)
+
+        self.optimise(gradient)
 
     def __str__(self):
         return f'Dense {self.name} size {self.size}  number_variable {self.number_variable}'
 
 
-model = Input((10,))
-model.add_layer(Dense(20))
+size = 20
+sample = 1000
+x = np.random.rand(size*sample).reshape((-1, size))
+y = np.matmul(x, np.ones((x.shape[1], 1)))
+shuffle_index = np.arange(sample)
+np.random.shuffle(shuffle_index)
 
-model.add_layer(Dense(40))
+x = x[shuffle_index]
+y = y[shuffle_index]
+
+model = Dense(size)
+model.add_layer(Dense(1))
 
 model.compile()
 model.summary()
 
-x = np.arange(10)
-y = model.forward(x)
-y.shape
+
+
+
+for index in range(len(x)):
+    sample = x[index]
+    y_true = y[index]
+    model.fit(sample, y_true)
+
+
+model.forward(np.ones(20)/10)
